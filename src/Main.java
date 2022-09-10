e28547bde7a28f3c94dc5deb6401b2cacb9dcd0d
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-10 15:08:42 +0300

    comment-no

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class Main {
+    public static final Random ran = new Random();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            list.add( ran.nextInt(0, 2));
+            Collections.sort(list);
+            System.out.println(list);
+        }
+        System.out.println(" ");
+        LinkedList<Integer> linkedList = new LinkedList<>(list);
+        for (int i = 0; i < linkedList.size(); i++) {;
+            Collections.sort(linkedList);
+            System.out.println(linkedList);
+        }
     }
 }
